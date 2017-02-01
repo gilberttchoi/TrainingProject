646baf6b3ae368015e53d68bc49ad5b8f948cd23
gilberttchoi <gilbert.tchoi@supelec.fr>
2017-02-01 16:16:41 +0100

    Quelques fonctionnalites pour l API

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -4,8 +4,8 @@ $factory->define(App\Task::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'title' => str_random(10),
+        'title' => $faker->realText(rand(10,40)),
         'deadline' => $faker->date($format = 'Y-m-d', $max = 'now'),
-        'detail' => str_random(50),
+        'detail' => $faker->realText(rand(100,200)),
     ];
 });
